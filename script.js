9058e8c40e9167fb67bfe05754db8ec9fa3902c2
Madan Mohan Reddy <119772090+1-MadanMohan@users.noreply.github.com>
2025-08-05 12:46:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+ function checkAge() {
+      let age = document.getElementById("age").value;
+
+      let promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+          if (age >= 18) {
+            resolve("Welcome. You can vote.");
+          } else {
+            reject("Oh sorry. You aren't old enough.");
+          }
+        }, 4000); // 4 second delay
+      });
+
+      promise
+        .then((message) => alert(message))
+        .catch((error) => alert(error));
+    }
